3f73a64f14ee57c28de4de9b5e1aefe1e7886fc2
Owen Phillips <ophilli@clemson.edu>
2017-06-21 03:44:16 -0400

    implement event logging in on_off

--- Software/src/on_off.py
+++ Software/src/on_off.py
@@ -5,6 +5,7 @@ import buzzer
 import led
 import os
 import configparser
+import sql
 
 config = configparser.RawConfigParser() #instantiate config reader
 config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config.cfg')) #actually read the config file
@@ -17,12 +18,14 @@ io.setwarnings(False)
 io.setup(power, io.OUT)
 io.output(power, False) # Make sure Powerswitch Tail is off
 
-def TurnPowerOff():
+def TurnPowerOff(ID):
     io.output(power, False)
+    eventLog(ID, 2)
     piezo.play(5)
     led.off()
 
-def TurnPowerOn():
+def TurnPowerOn(ID):
     io.output(power, True)
+    eventLog(ID, 0)
     piezo.play(1)
     led.on()
