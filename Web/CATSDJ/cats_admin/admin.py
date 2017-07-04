10901d45b56950de686009053431ce3bfaa4c5b3
Owen Phillips <ophilli@clemson.edu>
2017-07-03 21:03:06 -0400

    update CATSDJ model view

--- Web/CATSDJ/cats_admin/admin.py
+++ Web/CATSDJ/cats_admin/admin.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 
-from .models import User, Machine, Event
+from .models import Certification, User, Machine, Event
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'first_name', 'last_name', 'user_2fa', 'year_in_school')
+    list_display = ('username', 'first_name', 'last_name', 'cuid', 'rfid', 'affiliation', 'major', 'get_certs', 'join_date')
 
-class MachineAdmin(admin.ModelAdmin):
-    list_display = ('hostname', 'mach_type', 'mach_sn', 'mach_2fa')
+#class MachineAdmin(admin.ModelAdmin):
+    #list_display = ('hostname', 'mach_type', 'mach_sn', 'mach_2fa', 'get_certs')
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('mach_hostname', 'cuid', 'status', 'timestamp')
+    list_display = ('node', 'user', 'status', 'timestamp')
 
 admin.site.register(User, UserAdmin)
-admin.site.register(Machine, MachineAdmin)
+#admin.site.register(Machine, MachineAdmin)
 admin.site.register(Event, EventAdmin)
+admin.site.register(Certification)
 # Register your models here.
--- Web/CATSDJ/cats_admin/models.py
+++ Web/CATSDJ/cats_admin/models.py
@@ -2,53 +2,112 @@ import datetime
 
 from django.db import models
 
+class Certification(models.Model):
+    cert_name = models.CharField(max_length=30)
+    # Expiration datetime
+    # Prerequisite
+    def __str__(self):
+        return self.cert_name
+
+class Major(models.Model):
+    name = models.CharField(max_length=30)
+    code = models.CharField(max_length=30)
+
+    AGRICULTURE = 'CAFL'
+    ART = 'CAAH'
+    ENGINEERING = 'CECAS'
+    PROVOST = 'PROV'
+
+    COLLEGE_CHOICES = (
+        (AGRICULTURE, 'Agriculture, Foresty, Life Sciences'),
+        (ART, 'Art, Architecture, and Humanities'),
+        (ENGINEERING, 'Engineering, Computing and Applied Sciences'),
+        (PROVOST, 'Provost'),
+    )
+
+    college = models.CharField(
+        max_length = 4,
+        choices = COLLEGE_CHOICES,
+        default = ENGINEERING,
+    )
+
+    def get_student_count(self):
+        return -1 # the number of students in this major
+
 class User(models.Model):
-    username = models.CharField(max_length=30)
-    cuid = models.CharField(max_length=30)
-    t1String = models.CharField(max_length=11)
+    username = models.CharField(max_length=30)  # Used for emails
+    cuid = models.CharField(max_length=9, primary_key=True) # Shouldn't change - primary key? | Also good for looking up info
+    rfid = models.CharField(max_length=11)  # Necessary for RFID scanning
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    user_2fa = models.BooleanField()
+    join_date = models.DateTimeField('Date Registered') # Interesting user data?
+    cert_group = models.ManyToManyField(Certification)
+    major = models.ForeignKey(Major, on_delete=models.PROTECT) # Users can have one major, majors can have many users
 
     FRESHMAN = 'FR'
     SOPHOMORE = 'S0'
     JUNIOR = 'JR'
     SENIOR = 'SR'
-    YEAR_IN_SCHOOL_CHOICES = (
+    MASTERS = 'MA'
+    PHD = 'Ph'
+    FACULTY = 'FA'
+
+    AFFLILIATION_CHOICES = (
         (FRESHMAN, 'Freshman'),
         (SOPHOMORE, 'Sophomore'),
         (JUNIOR, 'Junior'),
         (SENIOR, 'Senior'),
+        (MASTERS, 'Masters'),
+        (PHD, 'PhD'),
+        (FACULTY, 'Faculty'),
     )
 
-    year_in_school = models.CharField(
+    affiliation = models.CharField(
         max_length = 2,
-        choices = YEAR_IN_SCHOOL_CHOICES,
-        default=FRESHMAN,
+        choices = AFFLILIATION_CHOICES,
+        default = FRESHMAN,
     )
+
+    def get_certs(self):
+        return "\n".join([c.cert_name for c in self.cert_group.all()])
 
     def __str__(self):
         return self.first_name + " " + self.last_name
 
+class Node(models.Model):
+    id = models.CharField(max_length = 30, primary_key=True)
+    name = models.CharField(max_length=30)
+    certs = models.ManyToManyField(Certification)
+
+class Space(models.Model):
+    node = models.OneToOneField(Node, on_delete=models.PROTECT, primary_key=True)
+    def get_machines(self):
+        return -1 # the list of machines in this space
+
 class Machine(models.Model):
-    hostname = models.CharField(max_length=30)
-    mach_type = models.CharField(max_length=30)
-    mach_sn = models.CharField(max_length=30)
-    mach_2fa = models.BooleanField()
+    node = models.OneToOneField(Node, on_delete=models.PROTECT, primary_key=True)
+    manufacturer = models.CharField(max_length=30)
+    model = models.CharField(max_length=30)
+    serial = models.CharField(max_length=30)
+    space = models.ForeignKey(Space, on_delete=models.PROTECT) # Protect the machines from being deleted if a space is deleted
+
+    def get_certs(self):
+        return "\n".join([c.cert_name for c in self.cert_group.all()])
 
     def __str__(self):
         return self.hostname + " " + self.mach_type
 
 class Event(models.Model):
-    mach_hostname = models.ForeignKey('Machine', on_delete=models.PROTECT)
-    cuid = models.ForeignKey('User', on_delete=models.PROTECT)
+    timestamp = models.DateTimeField('Time of Event', primary_key=True)
+    node = models.ForeignKey('Node', on_delete=models.PROTECT) # Protect the event form being deleted if a node is deleted
+    user = models.ForeignKey('User', on_delete=models.PROTECT)
 
     SUCCESS = 'SE'
     NOT_AUTH = 'NA'
     MACH_ERR = 'ME'
     RFID_NOT_FOUND = 'RNF'
     MULTIPLE_LOGIN = 'ML'
-    HOST_NOT_FOUND = 'HNF'
+    NODE_NOT_FOUND = 'HNF'
 
     STATUS_CHOICES = (
         (SUCCESS, 'Successful Entry'),
@@ -56,7 +115,7 @@ class Event(models.Model):
         (MACH_ERR, 'Machine Error'),
         (RFID_NOT_FOUND, 'Input Device Not Found'),
         (MULTIPLE_LOGIN, 'Multiple Login Attempt'),
-        (HOST_NOT_FOUND, 'Hostname Not Found'),
+        (NODE_NOT_FOUND, 'Node ID Not Recognized'),
     )
 
     status = models.CharField(
@@ -64,8 +123,6 @@ class Event(models.Model):
         choices = STATUS_CHOICES,
         default = SUCCESS,
     )
-
-    timestamp = models.DateTimeField('Time of Event')
 
     def __str__(self):
         return self.get_status_display() + " @ " + str(self.timestamp)
