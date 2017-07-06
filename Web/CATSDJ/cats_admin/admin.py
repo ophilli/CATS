fd81a6328b00f9edcd0ef121d0cee934a95ac44f
Owen Phillips <ophilli@clemson.edu>
2017-07-06 11:04:53 +0000

    Remove Node from Admin Panel

--- Web/CATSDJ/cats_admin/admin.py
+++ Web/CATSDJ/cats_admin/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 
-from .models import Certification, Major, User, Node, Space, Machine, Event
+from .models import Certification, Major, User, Space, Machine, Event
 
 admin.site.register(Certification)
 admin.site.register(Major)
 admin.site.register(User)
-admin.site.register(Node)
 admin.site.register(Space)
 admin.site.register(Machine)
 admin.site.register(Event)
