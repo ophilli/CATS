fb1e265ab3974f9ee85870dc5f1756588b5e6312
Owen Phillips <ophilli@clemson.edu>
2017-07-06 14:34:00 -0700

    Start implementing unique value flags in models

--- Web/CATSDJ/cats_admin/models.py
+++ Web/CATSDJ/cats_admin/models.py
@@ -46,13 +46,13 @@ class Major(models.Model):
         return -1 # the number of students in this major
 
 class User(models.Model):
-    username = models.CharField(max_length=30)  # Used for emails
+    username = models.CharField(max_length=30, unique=True)  # Used for emails
     cuid = models.CharField(max_length=9, primary_key=True) # Shouldn't change - primary key? | Also good for looking up info
-    rfid = models.CharField(max_length=11)  # Necessary for RFID scanning
+    rfid = models.CharField(max_length=11, unique=True)  # Necessary for RFID scanning
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    join_date = models.DateTimeField('Date Registered') # Interesting user data?
-    cert_group = models.ManyToManyField(Certification)
+    join_date = models.DateTimeField('Date Registered', auto_now_add=True) # Interesting user data?
+    cert_group = models.ManyToManyField(Certification, blank=True, default=None)
     major = models.ForeignKey(Major, on_delete=models.PROTECT) # Users can have one major, majors can have many users
 
     FRESHMAN = 'FR'
@@ -80,7 +80,7 @@ class User(models.Model):
     )
 
     def get_certs(self):
-        return "\n".join([c.cert_name for c in self.cert_group.all()])
+        return "\n|\n".join([c.name for c in self.cert_group.all()])
 
     def __str__(self):
         return self.first_name + " " + self.last_name
@@ -108,8 +108,8 @@ class Machine(Node):
         return "\n".join([c.cert_name for c in self.cert_group.all()])
 
 class Event(models.Model):
-    timestamp = models.DateTimeField('Time of Event', primary_key=True)
-    
+    timestamp = models.DateTimeField('Time of Event', auto_now_add=True, primary_key=True)
+
     node = models.ForeignKey('Node', on_delete=models.PROTECT)
     user = models.ForeignKey('User', on_delete=models.PROTECT)
 
