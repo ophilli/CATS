8cef4faf876fbe68950e98f49e7947f9f931e1e2
Owen Phillips <ophilli@clemson.edu>
2017-07-06 14:34:29 -0700

    expand user admin field settings

--- Web/CATSDJ/cats_admin/admin.py
+++ Web/CATSDJ/cats_admin/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 
 from .models import Certification, Major, User, Space, Machine, Event
 
-
 class UserAdmin(admin.ModelAdmin):
-    fields = ['first_name', 'last_name', 'username', 'cuid', 'rfid', 'major', 'cert_group']
+    fields = ['first_name', 'last_name', 'username', 'cuid', 'rfid', 'major', 'affiliation', 'cert_group']
+    list_display = ('__str__', 'cuid', 'major', 'get_certs')
+    list_filter = ['major',]
+    search_fields = ['username']
 
 admin.site.register(Certification)
 admin.site.register(Major)
