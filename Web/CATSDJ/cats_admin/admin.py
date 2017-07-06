6649096e8f094fabd53c3e83e73c7064f6309f49
Owen Phillips <ophilli@clemson.edu>
2017-07-06 04:26:48 -0700

    Sort User Admin Fields

--- Web/CATSDJ/cats_admin/admin.py
+++ Web/CATSDJ/cats_admin/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 
 from .models import Certification, Major, User, Space, Machine, Event
 
+
+class UserAdmin(admin.ModelAdmin):
+    fields = ['first_name', 'last_name', 'username', 'cuid', 'rfid', 'major', 'cert_group']
+
 admin.site.register(Certification)
 admin.site.register(Major)
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Space)
 admin.site.register(Machine)
 admin.site.register(Event)
